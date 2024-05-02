5b50ec8156a5a2f6ff94b85a3a75d0b434af8acf
Daniel Gurbin <daniel.g@msapps.mobi>
2024-05-02 08:12:50 +0300

    add auth services

--- src/auth/services.go
+++ src/auth/services.go
@@ -1,1 +1,13 @@
 package auth
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+func CreateServices(db *mongo.Database) *services {
+	return &services{
+		db: db,
+	}
+}
+
+type services struct {
+	db *mongo.Database
+}
